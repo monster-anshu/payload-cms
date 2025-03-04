a65d5a9a4b5a7c27d4fd429a3b329b9f910b95bb
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-04 11:00:03 +0000

    add name in users

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -9,5 +9,11 @@ export const Users: CollectionConfig = {
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+      defaultValue: 'Himanshu',
+    },
   ],
 }
