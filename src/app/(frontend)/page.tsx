8c35b351a9630a3b629d107809850147ef0ee2e0
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-09 16:33:29 +0000

    add container utils

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,31 @@
-import { Button } from '@/components/ui/button'
+import { getPayload } from 'payload'
 import React from 'react'
+import configPromise from '@payload-config'
 
 export default async function HomePage() {
+  const payload = await getPayload({ config: configPromise })
+
+  const posts = await payload.find({
+    collection: 'posts',
+    depth: 1,
+    limit: 12,
+    overrideAccess: false,
+    select: {
+      title: true,
+      slug: true,
+      categories: true,
+      meta: true,
+    },
+  })
+
   return (
-    <div>
-      <Button>hello</Button>
-    </div>
+    <main className="container">
+      <h1 className="text-4xl">Posts</h1>
+      <div className="py-2">
+        {posts.docs.map((doc) => {
+          return <div key={doc.id}>{doc.title}</div>
+        })}
+      </div>
+    </main>
   )
 }
